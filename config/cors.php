38aacf4c9f6f17d1f816125de31a1decc163a287
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-11 17:43:13 +0200

    allowed fix

--- config/cors.php
+++ config/cors.php
@@ -19,11 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173/'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['http://localhost:5173/'],
 
     'exposed_headers' => [],
 
